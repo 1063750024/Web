c6433dde99f8b70a589a24afd5baeaaf556a1c80
xiaodoufu <501804292@qq.com>
2017-11-26 10:46:49 +0800

    原生的Ajax实现验证用户名是否存在

---
+++ 09Ajax/src/com/xdf/action/ValidateServlet.java
@@ -0,0 +1,41 @@
+package com.xdf.action;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ValidateServlet")
+public class ValidateServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// 处理乱码
+		req.setCharacterEncoding("utf-8");
+		resp.setCharacterEncoding("utf-8");
+
+		// 接收用户通过ajax传递过来的用户名
+		String userName = req.getParameter("userName");
+		// 定义一个标记 用于前台接收
+		boolean flag = false;
+		if ("admin".equals(userName)) {
+			flag = true; // 证明数据库中存在
+		}
+		// 响应数据
+		PrintWriter pw = resp.getWriter();
+		pw.print(flag);
+		pw.close();
+	}
+
+}
